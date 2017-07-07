ce56a53b197f69fdf5e6368d5507517a7e424aa4
Keanu Lee <keanulee@google.com>
2017-07-07 15:07:43 -0700

    sw-precache-config

---
+++ sw-precache-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  staticFileGlobs: [
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-loader.js',
+    // '/images/*' // TODO: figure out if we should precache some/all/no images
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\/images\//,
+      handler: 'fastest'
+    },
+    {
+      urlPattern: /\/data\//,
+      handler: 'networkFirst'
+    }
+  ]
+};
